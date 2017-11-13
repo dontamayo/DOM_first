900797c5ab39690c3c6735677c7e124e37cac0f4
Don aldrich Tamayo <amazingmardon@gmail.com>
2017-11-13 11:53:41 -0500

    sample DOM, and name of my classmates

--- apps.js
+++ apps.js
@@ -2,3 +2,11 @@ var headingElement = document.getElementById("main-heading")
 console.log(headingElement.innerHTML);
 var newHeadingText = prompt('please provide a new heading:');
 headingElement.innerHTML = newHeadingText;
+
+
+// document.querySelector(' .fun').innerHTML = "<img src = http://subversify.com/wp-content/uploads/2017/04/chicken-011.jpg" alt = 'fun chicken'>"
+
+// document.querySelector('p') .addEventListener( 'click',  function(){
+//   document.querySelector(' .fun') .innerHTML = '<img src = 'http://subversify.com/wp-content/uploads/2017/04/chicken-011.jpg'  alt = fun chicken>'
+//
+// })
